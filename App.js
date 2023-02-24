f0749edd680e75c301b65101869622d1bd3ae3f7
Francisco Kahil <codexico@gmail.com>
2023-02-23 22:47:57 -0300

    ImageBackground experiments

--- App.js
+++ App.js
@@ -1,20 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  StyleSheet,
+  ImageBackground,
+  Image,
+  View,
+  Dimensions,
+  Text,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import backgroundImage from "./assets/488x488.png";
+
+const { height, width } = Image.resolveAssetSource(backgroundImage);
+console.log("  ~ height:", height);
+
+const { width: deviceWidth, height: deviceHeight } = Dimensions.get("screen");
+
+let imageHeight = height;
+if (deviceWidth < width) {
+  // aspect ratio
+  imageHeight = (deviceWidth / width) * height;
+}
+console.log("  ~ imageHeight:", imageHeight);
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.screen}>
+        <View style={styles.content}>
+          <Text>content firstcontent firstcontent first</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content</Text>
+          <Text>content last</Text>
+        </View>
+        <ImageBackground
+          style={styles.imageBackgroundBottom}
+          source={backgroundImage}
+          resizeMode="contain"
+        >
+          <View style={styles.content}>
+            <Text>content firstcontent firstcontent first</Text>
+            <Text>content</Text>
+            <Text>content</Text>
+            <Text>content</Text>
+            <Text>content</Text>
+            <Text>content</Text>
+            <Text>content</Text>
+            <Text>content last</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "blue",
+  },
+  content: {
+    flex: 1,
+  },
+  imageBackgroundTop: {
+    backgroundColor: "green",
+    flex: 1,
+    top: -imageHeight / 2,
+  },
+  imageBackgroundBottom: {
+    backgroundColor: "pink",
+    flex: 1,
+    // bottom: -imageHeight / 2,
   },
 });
